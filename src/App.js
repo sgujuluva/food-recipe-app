ebb95006d719c9584c4ff7f589741e9c1f4fa59f
sgujuluva <sanghee04@gmail.com>
2022-08-11 22:03:51 +0200

    form input and button handled

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@
 import './App.css';
 
 function App() {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <div className="App">
      <h1>Food Receipe Search</h1>
-     <form>
+     <form onSubmit={handleSubmit}>
       <input type="text" placeholder='Type the Ingredient'/>
       <select className="ingredient-labels" name="" id="">
         <option value="vegan">Vegan</option>
       </select>
-      <button>Search</button>
+      <button onChange={(e) => e.target.value }>Get Receipe</button>
      </form>
     </div>
   );
