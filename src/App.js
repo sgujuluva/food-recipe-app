d197ab2e4e21dc66d3e4da859db74ac8db3685c1
sgujuluva <sanghee04@gmail.com>
2022-08-11 21:56:36 +0200

    form with input and button

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     
+     <h1>Food Receipe Search</h1>
+     <form>
+      <input type="text" placeholder='Type the Ingredient'/>
+      <select className="ingredient-labels" name="" id="">
+        <option value="vegan">Vegan</option>
+      </select>
+      <button>Search</button>
+     </form>
     </div>
   );
 }
