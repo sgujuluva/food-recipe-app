8f244c8e8012700984cce00052f23619de54a024
sgujuluva <sanghee04@gmail.com>
2022-08-12 14:02:59 +0200

    styling header done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import "./App.css";
-//images
-import IngredientImage from "./image/home5.png";
+//hooks
+import { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+
+
   const handleSubmit = (e) => {
     e.preventDefault();
   };
 
+  const handleChange = (e) => {
+    e.preventDefault();
+    setInput(e.target.value);
+  };
+  console.log("the input is:", input);
+
   return (
     <div className="recipe-content">
-      <div className="header">       
-       <span> <h1>Food Recipe Search</h1> </span>
+      <div className="header">
+        <span>
+          {" "}
+          <h1>Food Recipe Search</h1>{" "}
+        </span>
       </div>
 
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Type the Ingredient" />
+        <input type="text" onChange={handleChange} value={input} placeholder="Type the Ingredient" />
         <select className="ingredient-labels" name="" id="">
           <option value="vegan">Vegan</option>
         </select>
-        <button onChange={(e) => e.target.value}>Get Recipe</button>
+        <button >Get Recipe</button>
       </form>
     </div>
   );
