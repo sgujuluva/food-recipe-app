dcefe3570972188e384017877952075ccb9aaf8c
sgujuluva <sanghee04@gmail.com>
2022-08-12 12:19:12 +0200

    header styled

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-
-import './App.css';
+import "./App.css";
 //images
-import IngredientImage from "./image/home5.png"
+import IngredientImage from "./image/home5.png";
 
 function App() {
-
   const handleSubmit = (e) => {
     e.preventDefault();
-  }
+  };
 
   return (
     <div className="recipe-content">
-<div className="header">
-  <img src={IngredientImage } alt="" />
-<h1>Food Recipe Search</h1>
-</div>
-    
-     <form onSubmit={handleSubmit}>
-      <input type="text" placeholder='Type the Ingredient'/>
-      <select className="ingredient-labels" name="" id="">
-        <option value="vegan">Vegan</option>
-      </select>
-      <button onChange={(e) => e.target.value }>Get Recipe</button>
-     </form>
+      <div className="header">       
+       <span> <h1>Food Recipe Search</h1> </span>
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Type the Ingredient" />
+        <select className="ingredient-labels" name="" id="">
+          <option value="vegan">Vegan</option>
+        </select>
+        <button onChange={(e) => e.target.value}>Get Recipe</button>
+      </form>
     </div>
   );
 }
