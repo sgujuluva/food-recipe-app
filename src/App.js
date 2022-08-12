9c24b828d7cee397343316bb8528c7e565e05b3a
sgujuluva <sanghee04@gmail.com>
2022-08-12 14:23:09 +0200

    edamam api key created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { useState } from "react";
 
 function App() {
   const [input, setInput] = useState("");
+let app_id="1c719840";
+let app_key="804c77a30e46d9113a63ba170f404f73";
+let url = "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free";
 
 
   const handleSubmit = (e) => {
