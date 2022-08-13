7bcb3e9ab38aa67f0f44fa5b382a9fcf7c561a87
sgujuluva <sanghee04@gmail.com>
2022-08-13 15:55:58 +0200

    ingredients

--- src/Recipe/index.jsx
+++ src/Recipe/index.jsx
@@ -4,13 +4,17 @@ import "./Recipe.css"
 function Recipe({ recipeProp }) {
   return (
     <div className="recipe-list">
-      <h1>List of Recipes with ingredients requested:</h1>
+      <h1>List of Recipes with ingredients:</h1>
       <p>
         {recipeProp.map((item, i) => (
           <li key={i}> 
           <p className = "label">{item.recipe.label}</p>
           <img src={item.recipe.image} /* onClick={() => window.open(item.recipe.url)} */alt="" /> 
-           <p>Ingrdients: <br/>{item.recipe.ingredientLines}</p> 
+           <p>Ingrdient Lines: <br/>{item.recipe.ingredientLines}</p> 
+           <p>Ingredients: <br/> {item.recipe.ingredients.text}
+           {item.recipe.ingredients.quantity}
+           {item.recipe.ingredients.measure}
+           {item.recipe.ingredients.food}</p>
           </li>
         ))}
       </p>
